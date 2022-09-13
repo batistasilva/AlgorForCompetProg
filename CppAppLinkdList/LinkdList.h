f6150109ec50151d1bdd865ffcc35be38605e1d6
batistasilva <batista.freebsd@gmail.com>
2022-09-13 14:26:50 -0300

    Just an update to LinkdList

--- CppAppLinkdList/LinkdList.h
+++ CppAppLinkdList/LinkdList.h
@@ -25,9 +25,58 @@ public:
 Node *head = NULL;
 Node *tail = NULL;
 
-void pushBack(int);
-void popFront();
-void printList();
+/**
+ * The pushBack function add a node at the end of the list. The 
+ * new node becomes the tail, and the next pointer of the node that
+ * was previously the tail is modified and points to the new node.
+ */
+void pushBack(int val) {
+    Node *newNode = new Node(val, NULL);
+    if (tail == NULL) {
+        head = newNode;
+    } else {
+        tail->next = newNode;
+    }
+    tail = newNode;
+}
+
+/**
+ * The popFront function removes the first element of the list, if
+ * there is any. The node associated to the next pointer of the head
+ * becomes the head, and the node that was previously the head is
+ * removed.
+ */
+void popFront() {
+    if (head != NULL) {
+        Node *nextNode = head->next;
+        delete (head);
+        head = nextNode;
+        if (head == NULL) {
+            tail = NULL;
+        }
+    }
+}
+
+/**
+ * The function printList as its name says print the whole list
+ * by iterating trough all its elements printing them one by one. If
+ * the list is empty prints the message ”– empty list –”.
+ */
+void printList() {
+    Node *curNode = head;
+    if (curNode == NULL) {
+        printf("-- empty list --\n");
+        return;
+    }
+    while (curNode != NULL) {
+        printf("%d", curNode->val);
+        curNode = curNode->next;
+        if (curNode != NULL) {
+            printf(" -> ");
+        }
+    }
+    printf("\n");
+}
 
 
 #endif /* LINKDLIST_H */
